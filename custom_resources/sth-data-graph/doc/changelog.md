f9660301e8f874f89a36a44d8a64f715f477b94c
chriteli1 <christelioglanidis@outlook.com>
2022-11-25 20:25:58 +0200

    Graphs widget v3.0 and v3.1

--- custom_resources/sth-data-graph/doc/changelog.md
+++ custom_resources/sth-data-graph/doc/changelog.md
@@ -3,3 +3,11 @@
 ## v2.4
 
 - Focus must be on the widget to get live updates (reduces lag, document.hidden not working).
+
+## v3.0
+
+- Change the way requests are being sent to proxy server (because of changes at the proxy server code).
+
+## v3.1
+
+- Focus doesn't have to be on the widget to get live updates. It just needs to be visible on screen.
--- custom_resources/sth-data-graph/js/lineChart.js
+++ custom_resources/sth-data-graph/js/lineChart.js
@@ -5,11 +5,16 @@
 	var pause_flag = false;
 	console.log("Start");
 	document.getElementById("pause").addEventListener("click", Pause_Live);
+	
 	function Pause_Live(){
 		pause_flag = !pause_flag;
 		document.getElementById("pause").innerHTML = pause_flag ? "Go Live" : "Pause Feed";
 		console.log("pause inside function: ", pause_flag);
 	}
+
+	var title = document.getElementById("entityId"); // This is needed to check if widget is visible atm
+
+
 
 	/*===For testing only, comment out when not in testing===*/
 	// var graph_input = {id : "Room2"};
@@ -21,9 +26,10 @@
 	//Find the id of the selected entity
 	var handlerEntityIdInput = function handlerEntityIdInput(graph_input) {
 		// console.log("Hidden: ", document.visibilityState);
-		
-		if (graph_input != null && graph_input.id != null && document.hasFocus()){
+		//console.log("Title hidden: ", $(title).is(":hidden"));
+		if (graph_input != null && graph_input.id != null && !$(title).is(":hidden")){
 			// console.log("not hidden");
+			
 			if(!pause_flag){
 				var entityId = graph_input.id;
 				g_entityId = entityId;
@@ -68,23 +74,95 @@
 		if (loadLocalData) {
 			return callback(rawTemperatureSamples); //return samples from samples.js
 		} else {
-
-			return $.ajax({
+			var data1, data2, data3, data4, data5 = "";
+			// url = "http://localhost:5000/";
+			// var req1 = $.get("url" + g_entityId + "/pm1", function(response){ data = response });
+			// var req2 = $.get("url" + g_entityId + "/pm2_5", function(response){ data += response });
+			// var req3 = $.get("url" + g_entityId + "/pm10", function(response){ data += response });
+			// var req4 = $.get("url" + g_entityId + "/rh", function(response){ data += response });
+			// var req5 = $.get("url" + g_entityId + "/temp", function(response){ data += response });
+			// $.when(req1, req2, req3, req4, req5).done(function() {
+			// 	data = data.replace(/'/g, '"');
+			// 	data = data.replace(/False/g, 'false');
+			// 	json_data = JSON.parse(data); 
+			// 	console.log(json_data);
+			// 	return callback(json_data);
+			// });
+			var data = "";
+			var req1 = $.ajax({
 				method: 'GET',
-				//Change this URL if you want to use your own sth-comet
-				//url: 'http://localhost:8666/STH/v1/contextEntities/type/room/id/Room2/attributes/temp',
-				url: 'http://localhost:5000/' + g_entityId,
-				//data: urlParams,
-				//headers: headers,
-				//dataType: 'str',
-				success: function(data) {
-					data = data.replace(/'/g, '"');
-					data = data.replace(/False/g, 'false');
-					json_data = JSON.parse(data); 
-					console.log(json_data);
-					return callback(json_data);
+				// dataType: "string",
+				url: 'http://localhost:5000/' + g_entityId + "/pm1",
+				success: function(response) {
+					data1 = JSON.stringify(response);
+					data1 = data1.slice(1, -1);
+					data1 = "[" + data1 + ", ";
+					// data = data1;
 				}
 			});
+			var req2 = $.ajax({
+				method: 'GET',
+				// dataType: "string",
+				url: 'http://localhost:5000/' + g_entityId + "/pm2_5",
+				success: function(response) {
+					data2 =  JSON.stringify(response);
+					data2 = data2.slice(1, -1);
+					data2 = data2 + ", ";
+					// data += data1;
+				}
+			});
+			var req3 = $.ajax({
+				method: 'GET',
+				// dataType: "string",
+				url: 'http://localhost:5000/' + g_entityId + "/pm10",
+				success: function(response) {
+					data3 =  JSON.stringify(response);
+					data3 = data3.slice(1, -1);
+					data3 = data3 + ", ";
+					// data += data1;
+				}
+			});
+			var req4 = $.ajax({
+				method: 'GET',
+				// dataType: "string",
+				url: 'http://localhost:5000/' + g_entityId + "/rh",
+				success: function(response) {
+					data4 =  JSON.stringify(response);
+					data4 = data4.slice(1, -1);
+					data4 = data4 + ", ";
+					// data += data1;
+				}
+			});
+			var req5 = $.ajax({
+				method: 'GET',
+				// dataType: "string",
+				url: 'http://localhost:5000/' + g_entityId + "/temp",
+				success: function(response) {
+					data5 =  JSON.stringify(response);
+					data5 = data5.slice(1, -1);
+					data5 = data5 + "]";
+					// data += data1;
+				}
+			});
+			$.when(req1, req2, req3, req4, req5).done(function(){
+				data = data1 + data2 + data3 + data4 + data5;
+				data = data.replace(/'/g, '"');
+				json_data = JSON.parse(data);
+				console.log(json_data);
+				// console.log(data);
+				return callback(json_data);
+			});
+			// return $.ajax({
+			// 	method: 'GET',
+			// 	url: 'http://localhost:5000/' + g_entityId + "/temp",
+			// 	success: function(data) {
+			// 		data = data.replace(/'/g, '"');
+			// 		data = data.replace(/False/g, 'false');
+			// 		json_data = JSON.parse(data); 
+			// 		console.log(json_data);
+			// 		// return callback(json_data);
+			// 	}
+			// });
 		}
 	}
 
@@ -161,12 +239,15 @@
 			var values = [], attrName = [];
 			if (units == "pm"){
 				
-				values[2] = data[data.length - 3].contextResponses[0].contextElement.attributes[0].values;
-				attrName[2] = data[data.length - 3].contextResponses[0].contextElement.attributes[0].name;
-				values[3] = data[data.length - 4].contextResponses[0].contextElement.attributes[0].values;
-				attrName[3] = data[data.length - 4].contextResponses[0].contextElement.attributes[0].name;
-				values[4] = data[data.length - 5].contextResponses[0].contextElement.attributes[0].values;
-				attrName[4] = data[data.length - 5].contextResponses[0].contextElement.attributes[0].name;
+				values[2] = data[0].value;
+				// attrName[2] = data[data.length - 3].contextResponses[0].contextElement.attributes[0].name;
+				attrName[2] = "PM1";
+				values[3] = data[1].value;
+				// attrName[3] = data[data.length - 4].contextResponses[0].contextElement.attributes[0].name;
+				attrName[3] = "PM2.5";
+				values[4] = data[2].value;
+				// attrName[4] = data[data.length - 5].contextResponses[0].contextElement.attributes[0].name;
+				attrName[4] = "PM10";
 
 				var samples = [
 					{
@@ -186,11 +267,13 @@
 				loadGraph(samples);
 			}
 			else{
-				values[0] = data[data.length - 1].contextResponses[0].contextElement.attributes[0].values;
-				attrName[0] = data[data.length - 1].contextResponses[0].contextElement.attributes[0].name;
-				values[1] = data[data.length - 2].contextResponses[0].contextElement.attributes[0].values;
-				attrName[1] = data[data.length - 2].contextResponses[0].contextElement.attributes[0].name;
-				
+				values[0] = data[3].value;
+				// attrName[0] = data[data.length - 1].contextResponses[0].contextElement.attributes[0].name;
+				attrName[0]= "RH";
+				values[1] = data[4].value;
+				// attrName[1] = data[data.length - 2].contextResponses[0].contextElement.attributes[0].name;
+				attrName[1] = "Temperature";
+
 				var samples = [
 					{
 						key: attrName[0],
